5c644083d424ef8155a767769a4e8a53002ed271
harshchopra <harshchopra30@gmail.com>
2021-07-08 21:16:20 +0530

    reaxt components

--- dummy-app/src/App.js
+++ dummy-app/src/App.js
@@ -1,16 +1,30 @@
+import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap'
 import './App.css';
+import Menu from './components/Menu';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-          <div className="contianer">
-            <NavbarBrand >Sample site</NavbarBrand>
-          </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+            <div className="contianer">
+              <NavbarBrand href="" >Sample site</NavbarBrand>
+            </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
